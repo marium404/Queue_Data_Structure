28b92c7ebdd6913cc5a07e110eb1b46d3497468d
Maryam Asghar <56351104+marium404@users.noreply.github.com>
2020-11-29 08:32:13 +0500

    Update Queue_arr.cpp

--- Queue_arr.cpp
+++ Queue_arr.cpp
@@ -1,85 +1,92 @@
 #include <iostream>
 using namespace std;
 
-int queue[100],n=100,front=- 1,rear=- 1;
+int queue[100], n = 100, front = -1, rear = -1;
 
 void enqueue(){
+	
 		int a;
-   		if (front==NULL)
+   		if (front == NULL)
    		{
-   			cout<<"Queue Overflow"<<endl;
+   			cout << "Queue Overflow\n";
    		}
    		else {
-     		 if (front==- 1)
+     		 if (front == -1)
       			front = 0;
-      			cout<<"Insert the element in queue : "<<endl;
-      			cin>>a;
+      			cout << "Insert the element in queue :\n ";
+      			cin >> a;
       			rear++;
-      			queue[rear]=a;
+      			queue[rear] = a;
   		 }
 }
 
 void dequeue(){
-   if (front==- 1 || front>rear) {
+   if (front == - 1 || front > rear) {
    	
-      cout<<"Queue Underflow";
+      cout << "Queue Underflow";
       
    } 
    else {
-   		cout<<"Element deleted from queue is:"<< queue[front] <<endl;
-      	front++;;
+   	cout << "Element deleted from queue is:" << queue[front] <<endl;
+      	front++;
    }
 }
 
 void display() {
-   if (front==-1)
+	
+   if (front == -1)
    {
-   		cout<<"\nQueue is empty"<<endl;
+   		cout << "\nQueue is empty\n";
    }
    else {
-      cout<<"Queue elements are:"<<endl;
+      cout << "Queue elements are:\n";
       
-      for (int i=front;i<=rear;i++)
+      for (int i = front; i <= rear; i++)
       {
-	  	cout<<queue[i]<<" ";
-        cout<<endl;
+	cout << queue[i] << " ";
+        cout << endl;
    	  }
    }
 }
 
 int main() {
 	
+	system("color 05");
+	
    int x;
-   cout<<"1) Insert element to queue"<<endl;
-   cout<<"2) Delete element from queue"<<endl;
-   cout<<"3) Display all the elements of queue"<<endl;
-   cout<<"4) Exit"<<endl;
+   cout << "1: Insert element to queue\n";
+   cout << "2: Delete element from queue\n";
+   cout << "3: Display all the elements of queue\n";
+   cout << "4: Exit\n"l;
    
    while(true){
    
-      cout<<"\nEnter choice:"<<endl;
-      cin>>x;
+      cout << "\nEnter choice:"<<endl;
+      cin >> x;
       switch (x) {
       	
          case 1:
-		  		enqueue();
-		  		display();
-         		break;
+		      
+		      enqueue();
+		      display();
+		      break;
          		
-         case 2: 
-		 		dequeue();
-		 		display();
-         		break;
+         case 2:
+		      
+		      dequeue();
+		      display();
+		      break;
          		
-         case 3: 
-		 		display();
-         		break;
+         case 3:
+		      
+		      display();
+		      break;
          case 4: 
-
-		 		exit(0);
+		      
+		      exit(0);
          		
          default: 
-		 		cout<<"Invalid choice"<<endl;
+		      cout << "Invalid choice\n";
       
 	  	}
    
